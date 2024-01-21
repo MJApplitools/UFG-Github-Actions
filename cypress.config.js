35405c9dcd02970a3bb0d87128e08d87b38cfa7a
mjapplitools <matt.jasaitis@applitools.com>
2024-01-21 09:36:12 -0800

    test

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      this.video =  false
+      this.video =  false,
+	  this.baseUrl = 'http://localhost:3000'
     },
   },
 });
-
 
 require('@applitools/eyes-cypress')(module); 
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,14 @@ describe('ACME Bank', () => {
     beforeEach(() => {
         cy.eyesOpen({
             appName: 'ACME Bank',
-            testName: 'Bank - Functional Test'
+            testName: 'Bank - Functional Test',
+			browser: [
+				{width: 800, height: 600, name: 'chrome'},
+				{width: 1600, height: 1200, name: 'firefox'},
+				{width: 1024, height: 768, name: 'safari'},
+				{deviceName: 'Pixel 2', screenOrientation: 'portrait'},
+				{deviceName: 'Nexus 10', screenOrientation: 'landscape'},
+			]
         })
     })
     it('should log into a bank account', () => {
